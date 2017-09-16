7e61cea3920f259702291ad04d550d887353a9a6
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-09-16 05:32:11 -0400

    Add tidepool import script

---
+++ data/tidepool_import.py
@@ -0,0 +1,13 @@
+import requests
+
+def tidepool_import(username, passwd):
+    #TODO(Wesley) deal with wrong passwd/errors
+    r = requests.post('https://api.tidepool.org/auth/login', auth=(username, passwd))
+    user_id = r.json()["userid"]
+    tidepool_session = r.headers["x-tidepool-session-token"]
+    headers = {
+        "x-tidepool-session-token": tidepool_session,
+        "Content-Type": "application/json"
+    }
+    r = requests.get('https://api.tidepool.org/data/{}'.format(user_id), headers=headers)
+    return r.json()
