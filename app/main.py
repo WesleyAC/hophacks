2097a523dbe0b203cac1e1135bd072e1b7c876a3
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-09-16 06:00:59 -0400

    Add tidepool login page

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ app = Flask(__name__, static_folder="static")
 def root():
     return app.send_static_file('index.html')
 
+@app.route('/login')
+def login():
+    return app.send_static_file('login.html')
+
 @app.route("/trends")
 def trends():
     return render_template('trends.html')
