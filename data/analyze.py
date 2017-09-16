696675babafc6ae23ba635e06415b552328b8cfc
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-09-16 02:34:45 -0400

    Stub out data analysis methods

---
+++ data/analyze.py
@@ -0,0 +1,17 @@
+def time_high(avg_data):
+    pass
+
+def time_low(avg_data):
+    pass
+
+def forgot_bolus(avg_data):
+    pass
+
+def bedtime_bg(avg_data):
+    pass
+
+def morning_bg(avg_data):
+    pass
+
+def forgot_correction(avg_data):
+    pass
