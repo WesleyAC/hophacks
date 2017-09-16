54c2983d5fd5397abedf51601f2f668cf1c308d3
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-09-15 22:48:22 -0400

    Add pumpsettings import function

--- data/main.py
+++ data/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class RawData:
     """
     Data including both CGM datapoings and boluses
@@ -21,7 +23,25 @@ class PumpSettings:
       * Correction
       * Carbs
     """
-    def __init__(self):
-        self.basel = []
-        self.bolus_carbs = []
-        self.bolus_correction = []
+
+    def __init__(self, tidepool_data):
+        """
+        Takes tidepool_data as a python object representing a tidepool
+        pumpSettings event.
+        """
+        assert tidepool_data["type"] == "pumpSettings"
+        self.basel = tidepool_data["basalSchedules"][tidepool_data["activeSchedule"]]
+        self.bolus_carbs = tidepool_data["basalSchedules"][tidepool_data["activeSchedule"]]
+        self.bolus_correction = tidepool_data["insulinSensitivities"][tidepool_data["activeSchedule"]]
+
+    @classmethod
+    def from_all_data(cls, data):
+        """
+        Takes all tidepool data as a python object and returns a PumpSettings
+        object that represents the most recent active profile.
+        """
+        settings = list(filter(lambda x: x["type"] == "pumpSettings", data))
+        # 2017-05-30T21:14:07
+        # %Y-%m-%jT%H:%M:%S
+        settings.sort(key = lambda x: datetime.strptime(x["deviceTime"], "%Y-%m-%jT%H:%M:%S"))
+        return settings[0]
